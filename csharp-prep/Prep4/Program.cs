3c958d69a5248e4cf42c5a933a3a1509aeb75ed1
lewisombaka <lewisochieng99@gmail.com>
2024-09-27 00:41:03 +0300

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List <int> listNumbers = new List<int>();
+        Console.WriteLine("Type 0 to exit");
+        int guess = 1;
+        int sum = 0;
+        while(guess != 0)
+        {   
+            Console.Write("Enter Number: ");
+            string guessUser = Console.ReadLine();
+            guess = int.Parse(guessUser);
+            listNumbers.Add(guess);
+        }
+        foreach(int li in listNumbers)
+        {
+            sum += li;
+
+        }
+
+        float average = sum / ((listNumbers.Count)-1); 
+        int max = listNumbers[0];
+        foreach(int li in listNumbers)
+        {
+            if (li > max)
+            {
+                max = li;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest numbers is: {max}");
+
     }
 }
